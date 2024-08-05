94e59203c9b6aa33bdbb604fc273d63005ff0ab2
Alberto <betim009@gmail.com>
2024-08-05 17:27:53 -0300

    update

--- apps/2_image_and_data/app.py
+++ apps/2_image_and_data/app.py
@@ -1,2 +1,26 @@
-"""
-"""
+import streamlit as st
+from data import pokemons
+
+
+# Função principal do aplicativo Streamlit
+# Função principal do aplicativo Streamlit
+def main():
+    st.title("Pokédex Streamlit")
+    st.write("Bem-vindo à Pokédex!")
+
+    # Definindo o número de colunas
+    num_columns = 3
+    columns = st.columns(num_columns)
+
+    # Adicionando os pokémons nas colunas
+    for index, pokemon in enumerate(pokemons):
+        col = columns[index % num_columns]
+        with col:
+            st.image(pokemon["url"], caption=pokemon["name"])
+            st.write(f"**Número:** {pokemon['number']}")
+            st.write(f"**Nome:** {pokemon['name']}")
+            st.write(f"**Tipos:** {', '.join(pokemon['type'])}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ apps/2_image_and_data/data.py
@@ -0,0 +1,74 @@
+pokemons = [
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+        'number': 1,
+        'name': 'Bulbasaur',
+        'type': ['Grass', 'Poison']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+        'number': 2,
+        'name': 'Ivysaur',
+        'type': ['Grass', 'Poison']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+        'number': 3,
+        'name': 'Venusaur',
+        'type': ['Grass', 'Poison']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+        'number': 4,
+        'name': 'Charmander',
+        'type': ['Fire']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png',
+        'number': 5,
+        'name': 'Charmeleon',
+        'type': ['Fire']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
+        'number': 6,
+        'name': 'Charizard',
+        'type': ['Fire', 'Flying']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png',
+        'number': 7,
+        'name': 'Squirtle',
+        'type': ['Water']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png',
+        'number': 8,
+        'name': 'Wartortle',
+        'type': ['Water']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png',
+        'number': 9,
+        'name': 'Blastoise',
+        'type': ['Water']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png',
+        'number': 10,
+        'name': 'Caterpie',
+        'type': ['Bug']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/011.png',
+        'number': 11,
+        'name': 'Metapod',
+        'type': ['Bug']
+    },
+    {
+        'url': 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png',
+        'number': 12,
+        'name': 'Butterfree',
+        'type': ['Bug', 'Flying']
+    }
+]
